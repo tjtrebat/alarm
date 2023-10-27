a1045898fdb001fc31a7cf805eeb46a4f8f205e1
Thomas J. Trebat <ttrebat3@gatech.edu>
2023-10-27 01:21:06 -0500

    Fixed bug with multiple alarm press

--- alarm.py
+++ alarm.py
@@ -15,10 +15,11 @@ class Alarm:
         self.frame = Frame(root, padx=10, pady=10)
         self.frame.pack()
         self.root = root
-        self.alarm_set_counter = 0 # variable used to blink alarm when being set
-        self.alarm_btn_press = 0 # a character index into self.alarm_set_times[0] use to change alarm time
+        self.alarm_set_counter = 0
+        self.alarm_btn_press = 0
         self.alarm_set_times = ["00:00", "  :  "]
-        self.is_set_alarm = True # used to check if alarm has been set
+        self.is_set_alarm = False
+        self.is_alarm_being_set = False
         self.after_method = None
         self.add_current_time()
         self.add_alarm_time()
@@ -31,7 +32,7 @@ class Alarm:
         Label(self.frame, text="Current Time").grid(row=0, column=4)
         self.current_time = Label(self.frame, text=time.strftime("%H:%M:%S"))
         self.current_time.grid(row=1, column=4)
-        self.current_time.after(200, self.tick) # change text to time every 200 ms
+        self.current_time.after(200, self.tick)
 
     def tick(self):
         """ Updates current time label. Play sound if alarm
@@ -39,8 +40,8 @@ class Alarm:
         alarm_time = time.strftime("%H:%M:%S")
         self.current_time.config(text=alarm_time)
         self.current_time.after(200, self.tick)
-        if self.is_set_alarm and self.after_method: # enter here if alarm has been set
-            if alarm_time[:-3] == self.alarm_set_times[0]: # play sound if alarm time is current time
+        if self.is_set_alarm:
+            if alarm_time[:-3] == self.alarm_set_times[0]:
                 self.create_sound()
                 self.is_set_alarm = False
                 
@@ -63,16 +64,17 @@ class Alarm:
 
     def num_click_handler(self, num):
         """ Event handler for button press """
-        if not self.is_set_alarm:
+        if self.is_alarm_being_set:
             alarm_time = self.alarm_set_times[0]
             alarm_time = alarm_time[:self.alarm_btn_press] + str(num) + alarm_time[self.alarm_btn_press + 1:]
             self.alarm_set_times[0] = alarm_time
-            self.alarm_btn_press += 2 if self.alarm_btn_press == 1 else 1  # move index past the semicolon
-            if self.alarm_btn_press >= 5:  # numbers pressed 4 times
-                self.alarm.after_cancel(self.after_method) # cancel alarm blink
-                self.alarm.config(text=self.alarm_set_times[0]) # set alarm time
-                self.alarm_btn_press = 0 # reset number of times number has been pressed
+            self.alarm_btn_press += 2 if self.alarm_btn_press == 1 else 1 
+            if self.alarm_btn_press >= 5:
+                self.alarm.after_cancel(self.after_method)
+                self.alarm.config(text=self.alarm_set_times[0])
+                self.alarm_btn_press = 0
                 self.is_set_alarm = True
+                self.is_alarm_being_set = False
 
     def add_alarm_button(self):
         """ Add button to set the alarm clock wake-up time """
@@ -82,15 +84,18 @@ class Alarm:
 
     def set_alarm_time(self):
         """ Make alarm time blink so the user knows it is being set """
+        if not self.is_alarm_being_set:
+            self.is_alarm_being_set = True
+            self.after_method = self.alarm.after(500, self.blink)
         self.is_set_alarm = False
-        self.after_method = self.alarm.after(500, self.blink)
     
     def blink(self):
         """ Recursive function used to blink the alarm
         when its time is being set """
-        self.alarm.config(text=self.alarm_set_times[self.alarm_set_counter % 2])
-        self.alarm_set_counter += 1
-        self.after_method = self.alarm.after(500, self.blink)
+        if self.is_alarm_being_set:
+            self.alarm.config(text=self.alarm_set_times[self.alarm_set_counter % 2])
+            self.alarm_set_counter += 1
+            self.after_method = self.alarm.after(500, self.blink)
 
     def add_pomodoro_buttons(self):
         frame = Frame(self.root)
@@ -105,19 +110,21 @@ class Alarm:
         Button(frame, text='Break Pomodoro', command=self.break_pomodoro).grid(row=7, column=1)
 
     def start_pomodoro(self):
-        if self.after_method:
+        if self.is_alarm_being_set:
             self.alarm.after_cancel(self.after_method)
+            self.is_alarm_being_set = False
         alarm_time = datetime.now() + timedelta(minutes=25)
-        self.alarm_set_times[0] = alarm_time.strftime("%H:%M:%S")
+        self.alarm_set_times[0] = alarm_time.strftime("%H:%M")
         self.alarm.config(text=self.alarm_set_times[0])
         self.alarm_btn_press = 0
         self.is_set_alarm = True
 
     def break_pomodoro(self):
-        if self.after_method:
+        if self.is_alarm_being_set:
             self.alarm.after_cancel(self.after_method)
+            self.is_alarm_being_set = False
         alarm_time = datetime.now() + timedelta(minutes=5)
-        self.alarm_set_times[0] = alarm_time.strftime("%H:%M:%S")
+        self.alarm_set_times[0] = alarm_time.strftime("%H:%M")
         self.alarm.config(text=self.alarm_set_times[0])
         self.alarm_btn_press = 0
         self.is_set_alarm = True        
